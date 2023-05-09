34442cb766c9600f01d8fccee988a0518a91992d
antoninodangelo11 <reapercsp@gmail.com>
2023-05-09 15:35:15 +0200

    add methods

--- assets/script-vue.js
+++ assets/script-vue.js
@@ -1,13 +1,49 @@
 const app = Vue.createApp({
     data() {
       return {
-        
+        newTodo: '',
+        inputError: false,
+        todos: [
+            {
+                text: "Fare la spesa",
+                done: false,
+            },
+            {
+                text: "Portare Jimmy a scuola",
+                done: true,
+            },
+            {
+                text: "Andare al mare",
+                done: false,
+            },
+            {
+                text: "Comprare l'orologio",
+                done: true,
+            },
+            {
+                text: "Chiamare il medico",
+                done: false,
+            },
+        ],
       };
     },
     methods: {
+        addTodo() {
+            let cleanedTodo = this.newTodo.trim();
+            if (cleanedTodo.length > 5 != '') {
+                this.todos.unshift(cleanedTodo);
+                this.newTodo = '';
+                this.inputError = false;
+            } else {
+                this.inputError = true;
+            }
+        },
 
+        deleteItem(index) {
+            this.todos.splice(index, 1);
+        }
     },
-  });
+});
   
   // Spazio lasciato di proposito
   
